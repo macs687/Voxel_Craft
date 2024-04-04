62459bd84e5cdfbf3f1e0cd3c4c1506605c03041
macs687 <macs38.hest@mail.ru>
2024-04-04 19:20:47 +0700

    первый треугольник

---
+++ src/MainSystem/Window.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+
+namespace MainSystem{
+    class Window{
+    public:
+        static void initialise(int WIDTH, int HEIGHT, const char* title);
+        static void destructor(GLFWwindow* Win_ptr);
+
+        Window(const Window &) = delete;
+        Window &operator=(const Window &) = delete;
+
+        static GLFWwindow* window_ptr;
+        const int width;
+        const int height;
+        static void terminate();
+        static void pollEvents();
+        static void swapBuffers(GLFWwindow* Win_ptr);
+        static bool isOpen(GLFWwindow* Win_ptr);
+        static void close(GLFWwindow* Win_ptr);
+        static void ClearColor(GLclampf red, GLclampf green, GLclampf blue, GLclampf alpha);
+        static void Clear();
+    private:
+        const char* windowName;
+    };
+}
---
+++ src/Main_app.hpp
@@ -0,0 +1,11 @@
+#include "MainSystem/Window.hpp"
+
+
+class Main_app{
+public:
+    int width = 1920;
+    int height = 1080;
+    const char* title = "Voxel_Craft";
+
+    void run();
+};
